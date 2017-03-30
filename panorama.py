3a599dc5657390b5071015724aca45021206fb04
TJ MacDougall <timothy.macdougall@uoit.net>
2017-03-29 20:13:20 -0400

    Stitches multiple images if given in order; no blending; heavy distortion

--- panorama.py
+++ panorama.py
@@ -3,55 +3,78 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
+def match(des1, des2):
+	bf = cv2.BFMatcher()
+	matches = bf.knnMatch(des1, des2, k=2)
+	# Apply ratio test
+	good = []
+	for m,n in matches:
+		if m.distance < 0.75*n.distance:
+			good.append(m)
+	return good
+
+def merge(img1, img2):
+	src1 = cv2.cvtColor(img1, cv2.COLOR_RGB2GRAY)
+	src2 = cv2.cvtColor(img2, cv2.COLOR_RGB2GRAY)
+
+	# Initialize SIFT detector
+	sift = cv2.SIFT()
+	# find keypoints and descriptors
+	kp1, des1 = sift.detectAndCompute(src1, None)
+	kp2, des2 = sift.detectAndCompute(src2, None)
+
+	matches = match(des1, des2)
+
+	dst_pts = np.float32([ kp1[m.queryIdx].pt for m in matches ])
+	src_pts = np.float32([ kp2[m.trainIdx].pt for m in matches ])
+
+	H, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
+	
+	proj2 = cv2.warpPerspective(img2, H, (img1.shape[1] + img2.shape[1], 
+									 img1.shape[0]))
+	
+	img3 = np.zeros(proj2.shape, dtype="uint8")
+	img3[0:img1.shape[0], 0:img1.shape[1]] = img1
+	#print proj2 > 0
+	img3[proj2 > 0] = proj2[proj2 > 0]
+	
+	return img3
+
+
+
 # Process command line args
 parser = argparse.ArgumentParser(
 	description='CSCI 4220U Project - Panorama Image Stitcher')
-parser.add_argument('imgfile1', help='Image file 1')
-parser.add_argument('imgfile2', help='Image file 2')
+parser.add_argument('imgfile', nargs='+', help='Image files')
 args = parser.parse_args()
 
-img1 = cv2.imread(args.imgfile1)
-img2 = cv2.imread(args.imgfile2)
-img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2RGB)
-img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2RGB)
-gray1 = cv2.cvtColor(img1, cv2.COLOR_RGB2GRAY)
-gray2 = cv2.cvtColor(img2, cv2.COLOR_RGB2GRAY)
+imgs = []
+for filename in args.imgfile:
+	image = cv2.imread(filename)
+	image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+	imgs.append(image)
 
-# Initialize SIFT detector
+'''
+kps = []
+descs = []
 sift = cv2.SIFT()
-# find keypoints and descriptors
-kp1, des1 = sift.detectAndCompute(gray1, None)
-kp2, des2 = sift.detectAndCompute(gray2, None)
 
-# Match using brute force matcher
-bf = cv2.BFMatcher()
-matches = bf.knnMatch(des1, des2, k=2)
+for img in imgs:
+	gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+	kp, desc = sift.detectAndCompute(gray, None)
+	kps.append(kp)
+	descs.append(desc)
+	'''
 
-# Ratio test
-good = []
-for m,n in matches:
-	if m.distance < 0.75*n.distance:
-		good.append(m)
-
-src_pts = np.float32([ kp2[m.trainIdx].pt for m in good ])
-dst_pts = np.float32([ kp1[m.queryIdx].pt for m in good ])
-
-H, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
-
-pano = cv2.warpPerspective(img2, H, (img1.shape[1] + img2.shape[1],
-									img1.shape[0]))
-pano[0:img1.shape[0], 0:img1.shape[1]] = img1
-
-#cv2.imshow("panorama", pano)
-#print 'Press any key to proceed'   
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()
+pano = imgs[0]
+for img in imgs[1:]:
+	pano = merge(pano, img)
 
 plt.figure(figsize=(10,10))
-plt.subplot(221)
-plt.imshow(img1)
-plt.subplot(222)
-plt.imshow(img2)
+for i in range(len(imgs)):
+	plt.subplot(2,len(imgs),i+1)
+	plt.imshow(imgs[i])
+
 plt.subplot(212)
 plt.imshow(pano)
 plt.show()
